b5000b260a32b9c4f5b62b59749f21710c107961
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-12-21 22:43:55 +0100

    Logic war added

--- Example_with_names/Example_with_names/Form1.Designer.cs
+++ Example_with_names/Example_with_names/Form1.Designer.cs
@@ -42,6 +42,7 @@
             button1.TabIndex = 0;
             button1.Text = "Einfache Abfrage";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -51,6 +52,7 @@
             button2.TabIndex = 1;
             button2.Text = "Mit Lambda-Ausdruck";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
@@ -60,6 +62,7 @@
             button3.TabIndex = 2;
             button3.Text = "Mit PLINQ";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // listBox1
             // 
--- Example_with_names/Example_with_names/Form1.cs
+++ Example_with_names/Example_with_names/Form1.cs
@@ -1,3 +1,4 @@
+
 namespace Example_with_names
 {
     public partial class Form1 : Form
@@ -6,5 +7,68 @@ namespace Example_with_names
         {
             InitializeComponent();
         }
+
+        //Das Strig-Array, welches wir mit LINQ abfragen wollen
+        private string[] namen =
+        {
+            "Müller",
+            "Meier",
+            "Kurbanov",
+            "Kurze",
+            "Schneider",
+            "Merkel",
+            "Krause",
+            "Baumann",
+            "Hofmann"
+        };
+
+        //Eine einfache LINQ-Abfrage:
+        /*
+         * Wir selektieren die Eintrge, die länger als fünf Zeichen sind, und sortieren das Ergebnis der Länge nach.
+         * Auf lokaler Ebene können wir von der sogennanten "Typinferenz" Gebrauch machen und den Ergebnis der Abfrage
+         * mit dem Schlüsselwort "var" anstatt mit "IEnumerable<string>" deklarieren
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var linqRes = from ps in namen
+                          where ps.Length > 5
+                          orderby ps.Length
+                          select ps;
+            Anzeigen(linqRes);
+        }
+
+        /*
+         * Dasselbe Ergebnis produziert die folgende Abfrage auf der Basis von Erweiterungsmethoden und
+         * Lambda-Ausdrücken
+         */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var linqRes = namen.Where(ps => ps.Length > 5).OrderBy(ps => ps.Length);
+            Anzeigen(linqRes);
+        }
+
+        /*
+         * Für sehr große Collections und Arrays bietet sich Parallel LINQ (PLINQ) an, welches Ihre
+         * Abfrage zwischen mehreren Threads aufteilt, um sie parallel zu verarbeiten.
+         * PLINQ aktivieren Sie, indem Sie die AsParallel-Methode auf das Array anwenden
+         */
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var linqRes = from ps in namen.AsParallel()
+                          where ps.Length > 5
+                          orderby ps.Length
+                          select ps;
+        }
+
+        /*
+         * Die folgende Hilfsmethode erwarted als Übergabeparameter ein IEnumerable<string>, 
+         * sie konvertiert die IEnumerable<string> in eine generische Liste vom Typ string und weist diese
+         * der ListBox als Datenquelle zu:
+         */
+        private void Anzeigen(IOrderedEnumerable<string> linqRes)
+        {
+            listBox1.DataSource = linqRes.ToList();
+        }
+
     }
 }
