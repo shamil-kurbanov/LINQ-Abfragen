8cd38cca95e2daebfbf985c2bc848431d75e6c4c
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-12-21 22:19:05 +0100

    GUI was added

--- Example_with_names/Example_with_names/Form1.Designer.cs
+++ Example_with_names/Example_with_names/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(36, 54);
+            button1.Name = "button1";
+            button1.Size = new Size(177, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Einfache Abfrage";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(36, 132);
+            button2.Name = "button2";
+            button2.Size = new Size(177, 42);
+            button2.TabIndex = 1;
+            button2.Text = "Mit Lambda-Ausdruck";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(36, 208);
+            button3.Name = "button3";
+            button3.Size = new Size(177, 42);
+            button3.TabIndex = 2;
+            button3.Text = "Mit PLINQ";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(262, 54);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(144, 199);
+            listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(463, 314);
+            Controls.Add(listBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "LINQ-Abfragen";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ListBox listBox1;
     }
 }
