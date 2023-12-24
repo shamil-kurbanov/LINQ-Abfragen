6dd7cf053a4d2b85dc7b8e8f9413b2691a115335
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-12-24 13:22:13 +0100

    Logic und functions were added

--- Stringabfrafgen_mit_LINQ/Stringabfrafgen_mit_LINQ/Form1.cs
+++ Stringabfrafgen_mit_LINQ/Stringabfrafgen_mit_LINQ/Form1.cs
@@ -6,5 +6,39 @@ namespace Stringabfrafgen_mit_LINQ
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "XY34/w = abc-12";
+            listBox1.Items.Add(s);
+            listBox1.Items.Add(String.Empty); listBox1.SelectedIndex = 0;
+
+            //Nur die Zeichen auswählen, die Zahlen darstellen
+            var query1 = from c in s
+                         where char.IsDigit(c)
+                         select c;
+
+            //Erst jetzt wird die Abfrage ausgeführt
+            foreach (char c in query1)
+            {
+                listBox1.Items.Add(c);
+                
+            }
+            listBox1.Items.Add(String.Empty);
+
+
+            //Die Count Methode:
+            int count = query1.Count();
+            listBox1.Items.Add($"Anzahl = {count}"); listBox1.Items.Add(String.Empty);
+
+            //Alle Zeichen vor dem ersten "=" auswählen:
+            var query2 = s.TakeWhile(c => c != '=');
+
+            //Die Zweite Abfrage ausführen:
+            foreach(char c in query2)
+            {
+                listBox1.Items.Add(c.ToString());
+            }
+        }
     }
 }
