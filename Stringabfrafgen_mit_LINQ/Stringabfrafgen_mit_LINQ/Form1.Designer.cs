2d0643d22c438cca89c062443fa7247d8b4bb222
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-12-24 13:08:27 +0100

    GUI was added

--- Stringabfrafgen_mit_LINQ/Stringabfrafgen_mit_LINQ/Form1.Designer.cs
+++ Stringabfrafgen_mit_LINQ/Stringabfrafgen_mit_LINQ/Form1.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(32, 33);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(190, 274);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(277, 249);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 58);
+            button1.TabIndex = 1;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(465, 379);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Button button1;
     }
 }
